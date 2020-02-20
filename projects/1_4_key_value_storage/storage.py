6b57070e09035c9b0ce00ce336ab8ff66de0382f
Nik Brezginov <n1k-n1k@inbox.ru>
2020-02-20 22:26:56 +0300

    key-values storage
    key-value хранилище.
    
    Данные сохраняются в файле storage.data.
    Добавление новых данных в хранилище и получение текущих значений осуществляется с помощью утилиты командной строки storage.py.
    
    Если при запуске утилиты переданы оба ключа, происходит добавление переданного значения по ключу и сохранение данных в файле.
    Если передано только имя ключа, происходит чтение файла хранилища и вывод на печать значений, которые были сохранены по данному ключу.

---
+++ projects/1_4_key_value_storage/storage.py
@@ -0,0 +1,64 @@
+'''
+Key-value хранилище
+
+Утилита может вызваться со следующими параметрами:
+Вывести на экран из хранилища значения по ключу:
+--key <имя ключа> , где <имя ключа> - ключ по которому получаются значения.
+Записать в хранилище значения по ключу:
+--key <имя ключа> --val <значение>, где <значение> - сохраняемое значение.
+'''
+
+import os
+import tempfile
+import json
+import argparse
+
+
+def storage_data_get():
+    storage_path = os.path.join(tempfile.gettempdir(), 'storage.data')
+    if os.path.exists(storage_path):
+        with open(storage_path, 'r') as f:
+            json_data = json.load(f)
+            return json_data
+
+
+def storage_data_put(data_dict):
+    storage_path = os.path.join(tempfile.gettempdir(), 'storage.data')
+    with open(storage_path, 'w') as f:
+        json.dump(data_dict, f)
+
+
+def val_add(k, v):
+    data = storage_data_get()
+    if k in data:
+        data[k].append(v)
+    else:
+        data[k] = [v]
+    storage_data_put(data)
+
+
+def val_get(k):
+    data = storage_data_get()
+    if k in data:
+        print(*data[k], sep=', ')
+        return data[k]
+    else:
+        print(None)
+        return None
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-k', '--key')
+    parser.add_argument('-v', '--val')
+    args = parser.parse_args()
+    k = args.key
+    v = args.val
+    if k and v:
+        val_add(k, v)
+    elif k:
+        val_get(k)
+
+
+if __name__ == '__main__':
+    main()

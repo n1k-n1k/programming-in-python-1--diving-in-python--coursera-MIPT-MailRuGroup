635909598f1b08091b7d1b11eb01514c25e74869
Nik Brezginov <n1k-n1k@inbox.ru>
2020-04-02 12:33:37 +0300

    All tests passed
    1. Установка соединения с сервером --> в конструктор класса Клиента.
    2. Дополнительная проверка невалидных ответов от сервера --> raise ClientError

--- projects/5_1_metrics_client/client.py
+++ projects/5_1_metrics_client/client.py
@@ -16,46 +16,53 @@ class Client:
         self._host = host
         self._port = port
         self._timeout = timeout
+        try:
+            self._sock = socket.create_connection((self._host, self._port), self._timeout)
+        except socket.error as err:
+            raise ClientError(err)
 
     def put(self, metric_key, metric_value, timestamp=None):
         timestamp = str(timestamp or int(time.time()))
         send_data = f'put {metric_key} {metric_value} {timestamp}\n'.encode('utf8')
 
-        with socket.create_connection((self._host, self._port), self._timeout) as sock:
-            try:
-                sock.sendall(send_data)
-                response = sock.recv(1024)
-                if b'ok\n' not in response:
-                    raise ClientError
-            except Exception:
+        try:
+            self._sock.sendall(send_data)
+            response = self._sock.recv(1024)
+            if b'ok\n' not in response:
                 raise ClientError
+        except Exception:
+            raise ClientError
 
     def get(self, key):
         metric_dict = {}
         send_data = f'get {key}\n'.encode('utf8')
-        with socket.create_connection((self._host, self._port), self._timeout) as sock:
-            try:
-                sock.sendall(send_data)
-                response = sock.recv(1024)
-                if b'ok' not in response:
-                    raise ClientError
-                response = str(response).strip('\n')
-                for m in response:
-                    metrics = m.split(' ')
-                    if len(metrics) == 3:
-                        metric_key = metrics[0]
-                        metric_value = float(metrics[1])
-                        timestamp = int(metrics[2])
-                        metric_list = metric_dict.get(metric_key, [])
-                        metric_list.append((timestamp, metric_value))
-                        metric_dict.update({metric_key: sorted(metric_list)})
-                return metric_dict
-            except Exception:
+
+        try:
+            self._sock.sendall(send_data)
+            response = self._sock.recv(1024)
+            if b'ok' not in response:
                 raise ClientError
+
+            response = str(response).strip('\n').split('\\n')
+
+            for m in response:
+                metrics = m.split(' ')
+                if len(metrics) == 3:
+                    metric_key = metrics[0]
+                    metric_value = float(metrics[1])
+                    metric_timestamp = int(metrics[2])
+                    metric_list = metric_dict.get(metric_key, [])
+                    metric_list.append((metric_timestamp, metric_value))
+                    metric_dict.update({metric_key: sorted(metric_list)})
+                elif metrics not in [["b'ok"], [""], ["'"]]:
+                    raise ClientError
+
+            return metric_dict
+
+        except Exception as err:
+            raise ClientError(err)
 
 
 if __name__ == '__main__':
     client = Client('127.0.0.1', 8888, timeout=15)
-    client.put('key1', 1.13, timestamp=70)
-    client.put('key1', 7.77)
     print(client.get('*'))
